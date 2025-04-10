b308c05a81e382a32011db6c9bd2fae7591b75fd
S-DPR <thankyouformuch0@gmail.com>
2025-04-10 14:19:04 +0900

    del: 멀티스레드 시도 흔적 제거

--- src/main/java/com/example/codestructure/transaction_sample/repos/EntityBRepos.java
+++ src/main/java/com/example/codestructure/transaction_sample/repos/EntityBRepos.java
@@ -1,22 +1,12 @@
 package com.example.codestructure.transaction_sample.repos;
 
 import com.example.codestructure.transaction_sample.entity.EntityB;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
-
-import static jakarta.persistence.LockModeType.PESSIMISTIC_READ;
 
 @Repository
 public interface EntityBRepos extends JpaRepository<EntityB, UUID> {
-    @Lock(PESSIMISTIC_READ)
-    @Query("SELECT b FROM EntityB b WHERE b.entityAId = :id")
-    List<EntityB> findByEntityAIdWithLock(UUID id);
+    boolean existsByEntityAId(UUID entityAId);
 }
