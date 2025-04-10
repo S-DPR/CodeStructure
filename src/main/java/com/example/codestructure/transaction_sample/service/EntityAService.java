dc9e5ee196f13781d16c995251999ed6802556d0
S-DPR <thankyouformuch0@gmail.com>
2025-04-10 14:20:57 +0900

    fix: EntityAService.findOrSave -> Lock 제거

--- src/main/java/com/example/codestructure/transaction_sample/service/EntityAService.java
+++ src/main/java/com/example/codestructure/transaction_sample/service/EntityAService.java
@@ -3,7 +3,6 @@ package com.example.codestructure.transaction_sample.service;
 import com.example.codestructure.transaction_sample.entity.EntityA;
 import com.example.codestructure.transaction_sample.repos.EntityARepos;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -33,17 +32,12 @@ public class EntityAService {
         entityARepos.deleteById(id);
     }
 
-    public Optional<EntityA> findByIdWithLock(UUID id) {
-        return entityARepos.findByIdWithLock(id);
-    }
-
-    @Transactional
     public EntityA findOrSave(EntityA entityA) {
         UUID id = entityA.getId();
         if (id == null) {
-            throw new IllegalArgumentException("EntityA ID must not be null.");
+            throw new IllegalArgumentException("EntityA ID가 비어있음");
         }
-        return entityARepos.findByIdWithLock(id)
+        return entityARepos.findById(entityA.getId())
                 .orElseGet(() -> entityARepos.save(entityA));
     }
 }
