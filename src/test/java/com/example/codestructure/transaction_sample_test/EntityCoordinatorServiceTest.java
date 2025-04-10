09474fbcbd5b9953970020cfb344c80407a49595
S-DPR <thankyouformuch0@gmail.com>
2025-04-10 15:31:09 +0900

    feat: 테스트코드 1개 더 추가 (같은 A를 가진 B를 두개 넣기)

--- src/test/java/com/example/codestructure/transaction_sample_test/EntityCoordinatorServiceTest.java
+++ src/test/java/com/example/codestructure/transaction_sample_test/EntityCoordinatorServiceTest.java
@@ -6,7 +6,6 @@ import com.example.codestructure.transaction_sample.entity.EntityB;
 import com.example.codestructure.transaction_sample.repos.EntityARepos;
 import com.example.codestructure.transaction_sample.repos.EntityBRepos;
 import com.example.codestructure.transaction_sample.service.EntityCoordinatorService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -30,7 +29,7 @@ class EntityCoordinatorServiceTest {
     EntityBRepos entityBRepos;
 
     @Test
-    void save_성공_트랜잭션_동작확인() {
+    void save_성공_동시에_넣기_동작확인() {
         // given
         EntityA entityA = new EntityA();
         entityA.setData(123);
@@ -52,6 +51,48 @@ class EntityCoordinatorServiceTest {
 
         assertThat(entityARepos.existsById(entityAId)).isTrue();
         assertThat(entityBRepos.existsById(entityBId)).isTrue();
+        assertThat(entityBRepos.findById(entityBId).get().getEntityAId()).isEqualTo(entityAId);
+    }
+
+    @Test
+    void save_성공_A가_같은_요소를_동시에_두개_넣기_동작확인() {
+        // given
+        UUID prepareEntityAId = UUID.randomUUID();
+        EntityA entityA = new EntityA();
+        entityA.setId(prepareEntityAId);
+        entityA.setData(123);
+
+        EntityB entityB = new EntityB();
+        entityB.setData(456);
+
+        EntityB entityB2 = new EntityB();
+        entityB2.setData(789);
+
+        Entities entities = new Entities(entityA, entityB);
+        Entities entities2 = new Entities(entityA, entityB2);
+
+        // when
+        Entities saved = coordinatorService.save(entities);
+        Entities saved2 = coordinatorService.save(entities2);
+
+        // then
+        UUID entityAId = saved.getEntityA().getId();
+        UUID entityBId = saved.getEntityB().getId();
+
+        UUID entityA2Id = saved2.getEntityA().getId();
+        UUID entityB2Id = saved2.getEntityB().getId();
+
+        assertThat(entityAId).isNotNull();
+        assertThat(entityBId).isNotNull();
+        assertThat(entityB2Id).isNotNull();
+
+        assertThat(entityA2Id).isEqualTo(entityA2Id);
+        assertThat(entityAId).isEqualTo(prepareEntityAId);
+        assertThat(entityA2Id).isEqualTo(prepareEntityAId);
+
+        assertThat(entityARepos.existsById(entityAId)).isTrue();
+        assertThat(entityBRepos.existsById(entityBId)).isTrue();
+        assertThat(entityBRepos.existsById(entityB2Id)).isTrue();
         assertThat(entityBRepos.findById(entityBId).get().getEntityAId()).isEqualTo(entityAId);
     }
 
