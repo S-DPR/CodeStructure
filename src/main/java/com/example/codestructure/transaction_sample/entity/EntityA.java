57896afc6c423a17f50e3e88ef420aa0c97cc55f
S-DPR <thankyouformuch0@gmail.com>
2025-04-10 16:01:07 +0900

    feat: EntityA에 GeneratedValue 추가

--- src/main/java/com/example/codestructure/transaction_sample/entity/EntityA.java
+++ src/main/java/com/example/codestructure/transaction_sample/entity/EntityA.java
@@ -1,6 +1,8 @@
 package com.example.codestructure.transaction_sample.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
@@ -14,6 +16,7 @@ import java.util.UUID;
 @ToString
 public class EntityA {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     UUID id;
     Integer data;
 }
