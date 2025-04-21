bb27b6f7fa6851c0792fb8997659e0dc3ce11e04
S-DPR <thankyouformuch@naver.com>
2025-04-21 14:07:05 +0900

    refactoring: EntityA -> Parent, EntityB -> Child로 이름 명확하게 수정

--- src/test/java/com/example/codestructure/transaction_sample_test/TransactionTest.java
+++ src/test/java/com/example/codestructure/transaction_sample_test/TransactionTest.java
@@ -17,7 +17,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 @SpringBootTest
-class CoordinatorServiceTest {
+class TransactionTest {
 
     @Autowired
     CoordinatorService coordinatorService;
