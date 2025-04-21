4558360e792765611d6c46981848154b2a74f0e5
S-DPR <thankyouformuch@naver.com>
2025-04-21 14:14:22 +0900

    fix: 변수 명 변경

--- src/main/java/com/example/codestructure/transaction_sample/service/CoordinatorService.java
+++ src/main/java/com/example/codestructure/transaction_sample/service/CoordinatorService.java
@@ -43,8 +43,8 @@ public class CoordinatorService {
     public void delete(Entities entities) {
         entities = prepareEntitiesFKField(entities);
         childService.deleteById(entities.getChild().getId());
-        boolean isUsing = childService.existsByEntityAId(entities.getChild().getParentId());
-        if (!isUsing) {
+        boolean isParentUsing = childService.existsByEntityAId(entities.getChild().getParentId());
+        if (!isParentUsing) {
             parentService.deleteById(entities.getParent().getId());
         }
     }
